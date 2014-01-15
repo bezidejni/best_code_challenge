9b06806726bb859bcac4559cc0464d25517ca7ca
Filip Jukic <filip@jukic.me>
2014-01-15 14:32:07 +0100

    more fixes for the import

--- best_api/movies/utils.py
+++ best_api/movies/utils.py
@@ -11,8 +11,12 @@ def import_movies_from_csv(path='movies.csv'):
         line = line.strip()
         movie_id, movie_title = line.split(",", 1)
         movie_title = normalize_movie_name(movie_title)
-        movie, created = Movie.objects.get_or_create(id=int(movie_id), title=movie_title)
-        if created:
+        try:
+            movie = Movie.objects.get(id=movie_id)
+            movie.title = movie_title
+            movie.save()
+        except Movie.DoesNotExist:
+            Movie.objects.create(id=movie_id, title=movie_title)
             new_movies_imported += 1
     return new_movies_imported
 

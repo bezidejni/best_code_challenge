e6a081c594a63270abab128ec2988f3178ea1ec5
Filip Jukic <filip@jukic.me>
2014-01-15 19:13:21 +0100

    show fuller URL for poster image

--- best_api/movies/serializers.py
+++ best_api/movies/serializers.py
@@ -3,6 +3,8 @@ from .models import Movie
 
 
 class MovieSerializer(serializers.HyperlinkedModelSerializer):
+    poster = serializers.Field(source='poster.url')
+
     class Meta:
         model = Movie
         fields = ('id', 'title', 'year', 'genre', 'imdb_rating', 'imdb_id', 'runtime', 'plot', 'poster')

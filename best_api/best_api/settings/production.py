fdd9403c35f8b537e1fd508e3463c7df06e4d335
Filip Jukic <filip@jukic.me>
2014-01-15 18:28:24 +0100

    added production db settings

--- best_api/best_api/settings/production.py
+++ best_api/best_api/settings/production.py
@@ -1,1 +1,18 @@
+import os
 from .base import *
+
+
+ALLOWED_HOSTS = [
+    '*.jukic.me',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'best_api',
+        'USER': 'api',
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}

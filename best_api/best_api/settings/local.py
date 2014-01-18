f700ab6f210d6774276b75617020956b9a02d01a
Filip Jukic <filip@jukic.me>
2014-01-18 10:59:53 +0100

    added some helpful apps for development

--- best_api/best_api/settings/local.py
+++ best_api/best_api/settings/local.py
@@ -1,1 +1,15 @@
 from .base import *
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake'
+    }
+}
+
+THIRD_PARTY_APPS += (
+    'django_extensions',
+    'debug_toolbar',
+)
+
+INSTALLED_APPS = DEFAULT_APPS + THIRD_PARTY_APPS + LOCAL_APPS

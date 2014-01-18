cf4160b4b8d4989b43558ed4e828f357214f1a0f
Filip Jukic <filip@jukic.me>
2014-01-18 13:15:09 +0100

    now serving media files in development

--- best_api/best_api/urls.py
+++ best_api/best_api/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from rest_framework import routers
@@ -9,10 +10,16 @@ router = routers.DefaultRouter()
 router.register(r'movies', views.MovieViewSet)
 
 urlpatterns = patterns('',
-
     url(r'^api', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^movies/(?P<pk>[\d]+)/$', views.MovieDetail.as_view(), name='movie-detail'),
-    url(r'^', views.MovieList.as_view(), name="movie-list"),
+    url(r'^$', views.MovieList.as_view(), name="movie-list"),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+   )

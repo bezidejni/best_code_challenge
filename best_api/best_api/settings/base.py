7d9e1488b79a716ede6c6df627890e5f36f09e4e
Filip Jukic <filip@jukic.me>
2014-01-18 12:50:48 +0100

    flatui template added

--- best_api/best_api/settings/base.py
+++ best_api/best_api/settings/base.py
@@ -94,8 +94,12 @@ TEMPLATE_DIRS = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

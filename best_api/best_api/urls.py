ed70205bfe34fcf0687f9c5c8071cfe08b5ddc3a
Filip Jukic <filip@jukic.me>
2014-01-18 11:13:24 +0100

    simple movie list view added

--- best_api/best_api/urls.py
+++ best_api/best_api/urls.py
@@ -9,7 +9,9 @@ router = routers.DefaultRouter()
 router.register(r'movies', views.MovieViewSet)
 
 urlpatterns = patterns('',
-    url(r'^', include(router.urls)),
+
+    url(r'^api', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', views.MovieList.as_view(), name="movie_list"),
 )
--- best_api/movies/views.py
+++ best_api/movies/views.py
@@ -1,6 +1,14 @@
 from rest_framework import viewsets
+from django.views.generic import ListView
 from .models import Movie
 from .serializers import MovieSerializer
+
+
+class MovieList(ListView):
+    template_name = "index.html"
+    queryset = Movie.objects.all()
+    paginate_by = 10
+    context_object_name = "movies"
 
 
 class MovieViewSet(viewsets.ReadOnlyModelViewSet):

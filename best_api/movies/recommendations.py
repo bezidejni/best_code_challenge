699245010978983865a510795509af969e56363d
Filip Jukic <filip@jukic.me>
2014-01-21 22:21:08 +0100

    added work in progress on recommendations

--- best_api/movies/recommendations.py
+++ best_api/movies/recommendations.py
@@ -1,189 +1,177 @@
 import cPickle as pickle
 from math import sqrt
+from statsmodels.tools.eval_measures import meanabs, rmse
 from django.core.cache import cache
 
-# Returns the Pearson correlation coefficient for p1 and p2
-def sim_pearson(prefs, p1, p2):
-    # Get the list of mutually rated items
-    si = {}
-    for item in prefs[p1]:
-        if item in prefs[p2]:
-            si[item] = 1
 
-    # if they are no ratings in common, return 0
-    if len(si) == 0:
-        return 0
+class ItemBasedRecommender(object):
 
-    # Sum calculations
-    n = len(si)
+    def __init__(self, **kwargs):
+        defaults = {
+            'damp_factor': 1,
+            'min_num_of_ratings': 5,
+            'num_of_results': 10,
+            'similar_calculate': 30,
+            'similarity_function': 'sim_cosine',
+            'training_set_percent': 80,
+        }
+        self.averages = {}
+        self.training_set = {}
+        self.test_set = {}
+        self.similarity_matrix = {}
 
-    # Sums of all the preferences
-    sum1 = sum([prefs[p1][it] for it in si])
-    sum2 = sum([prefs[p2][it] for it in si])
+        # save parameters from kwargs or use defaults
+        for (key, default_val) in defaults.iteritems():
+            setattr(self, key, kwargs.get(key, default_val))
 
-    # Sums of the squares
-    sum1Sq = sum([pow(prefs[p1][it], 2) for it in si])
-    sum2Sq = sum([pow(prefs[p2][it], 2) for it in si])
+    def getRecommendedItems(self, user):
+        userRatings = user
+        scores = {}
+        totalSim = {}
+        #itemMatch = transformPrefs(itemMatch)
+        # Loop over items rated by this user
+        for (item, rating) in userRatings.items():
 
-    # Sum of the products
-    pSum = sum([prefs[p1][it] * prefs[p2][it] for it in si])
+            # Loop over items similar to this one
+            for (similarity, item2) in self.similarity_matrix[item]:
 
-    # Calculate r (Pearson score)
-    num = pSum - (sum1 * sum2 / n)
-    den = sqrt((sum1Sq - pow(sum1, 2) / n) * (sum2Sq - pow(sum2, 2) / n))
-    if den == 0:
-        return 0
+                # Ignore if this user has already rated this item
+                if item2 in userRatings:
+                    continue
+                # Weighted sum of rating times similarity
+                scores.setdefault(item2, 0)
+                print similarity
+                scores[item2] += (similarity * rating)
+                # Sum of all the similarities
+                totalSim.setdefault(item2, 0)
+                totalSim[item2] += similarity
+        # Divide each total score by total weighting to get an average
+        rankings = [((score / totalSim[item]), item) for item, score in scores.items()]
+        rankings.sort(reverse=True)
+        return rankings
 
-    r = num / den
+    def evaluate(self):
+        pass
 
-    damp=1
-    if len(si) < 25:
-        damp = len(si) / 25.0
+    # Returns the best matches for person from the prefs dictionary.
+    # Number of results and similarity function are optional params.
+    def topMatches(self, person):
+        similarity = getattr(self, self.similarity_function)
+        if not self.averages:
+            self.calculate_averages()
+        scores = [(similarity(person, other), other)
+                  for other, ratings in self.training_set.iteritems()
+                  if other != person and len(ratings) > self.min_num_of_ratings]
+        scores.sort()
+        scores.reverse()
+        return scores[0:self.num_of_results]
 
-    return r*damp
+    def calculateSimilarItems(self):
+        # Create a dictionary of items showing which other items they
+        # are most similar to.
+        #prefs = self.transformPrefs()
+        # Invert the preference matrix to be item-centric
+        #prefs = transformPrefs(prefs)
+        c = 0
+        for item in self.training_set:
+            # Status updates for large datasets
+            c += 1
+            if c % 100 == 0:
+                print "%d / %d" % (c, len(self.training_set))
+            # Find the most similar items to this one
+            scores = self.topMatches(item)
+            self.similarity_matrix[item] = scores
+        return self.similarity_matrix
 
+    def calculate_averages(self):
+        inverted = self.transformPrefs()
+        for user, ratings in inverted.iteritems():
+            if not user in self.averages:
+                total_ratings = sum([rating for title, rating in ratings.iteritems()])
+                total_items = len(ratings)
+                self.averages[user] = total_ratings / total_items
 
-def transformPrefs(prefs):
-    result = {}
-    for person in prefs:
-        for item in prefs[person]:
-            result.setdefault(item, {})
+    def sim_pearson(self, p1, p2):
+        # Returns the Pearson correlation coefficient for p1 and p2
+        # Get the list of mutually rated items
+        si = {}
+        for item in self.training_set[p1]:
+            if item in self.training_set[p2]:
+                si[item] = 1
 
-            # Flip item and person
-            result[item][person] = prefs[person][item]
-    return result
+        # if they are no ratings in common, return 0
+        if len(si) == 0:
+            return 0
 
+        # Sum calculations
+        n = len(si)
 
-averages = {}
+        # Sums of all the preferences
+        sum1 = sum([self.training_set[p1][it] for it in si])
+        sum2 = sum([self.training_set[p2][it] for it in si])
 
+        # Sums of the squares
+        sum1Sq = sum([pow(self.training_set[p1][it], 2) for it in si])
+        sum2Sq = sum([pow(self.training_set[p2][it], 2) for it in si])
 
-def calculate_averages(prefs):
-    inverted = transformPrefs(prefs)
-    for user, ratings in inverted.iteritems():
-        if not user in averages:
-            total_ratings = sum([rating for title, rating in ratings.iteritems()])
-            total_items = len(ratings)
-            averages[user] = total_ratings / total_items
+        # Sum of the products
+        pSum = sum([self.training_set[p1][it] * self.training_set[p2][it] for it in si])
 
+        # Calculate r (Pearson score)
+        num = pSum - (sum1 * sum2 / n)
+        den = sqrt((sum1Sq - pow(sum1, 2) / n) * (sum2Sq - pow(sum2, 2) / n))
+        if den == 0:
+            return 0
 
-def sim_cosine(prefs, p1, p2):
-    user1 = []
-    user2 = []
-    for user in prefs[p1]:
-        if user in prefs[p2]:
-            user1.append(prefs[p1][user] - averages[user])
-            user2.append(prefs[p2][user] - averages[user])
-    brojnik = sum([prvi * drugi for prvi, drugi in zip(user1, user2)])
-    nazivnik = sqrt(sum([pow(prvi, 2) for prvi in user1])) * sqrt(sum([pow(drugi, 2) for drugi in user2]))
-    if nazivnik == 0.0:
-        return 0.0
-    rezultat = brojnik / nazivnik
-    damp=1
-    if len(user1) < 50:
-        damp = len(user1) / 50.0
-    return rezultat*damp
+        r = num / den
 
+        damp = 1
+        if len(si) < self.damp_factor:
+            damp = len(si) / float(self.damp_factor)
 
-# Returns the best matches for person from the prefs dictionary. 
-# Number of results and similarity function are optional params.
-def topMatches(prefs, person, n=5, similarity=sim_cosine):
-    if not averages:
-        calculate_averages(prefs)
-    scores = [(similarity(prefs, person, other), other)
-              for other, ratings in prefs.iteritems() if other != person and len(ratings) > 5]
-    scores.sort()
-    scores.reverse()
-    return scores[0:n]
+        return r*damp
 
-# Gets recommendations for a person by using a weighted average
-# of every other user's rankings
-def getRecommendations(prefs, person, similarity=sim_pearson):
-    totals = {}
-    simSums = {}
-    best_critics = topMatches(prefs, person, n=50)
-    for sim, other in best_critics:
-        for item in prefs[other]:
+    def sim_cosine(self, p1, p2):
+        user1 = []
+        user2 = []
+        for user in self.training_set[p1]:
+            if user in self.training_set[p2]:
+                user1.append(self.training_set[p1][user] - self.averages[user])
+                user2.append(self.training_set[p2][user] - self.averages[user])
+        brojnik = sum([prvi * drugi for prvi, drugi in zip(user1, user2)])
+        nazivnik = sqrt(sum([pow(prvi, 2) for prvi in user1])) * sqrt(sum([pow(drugi, 2) for drugi in user2]))
+        if nazivnik == 0.0:
+            return 0.0
+        rezultat = brojnik / nazivnik
+        damp = 1
+        if len(user1) < self.damp_factor:
+            damp = len(user1) / float(self.damp_factor)
+        return rezultat*damp
 
-            # only score movies I haven't seen yet
-            if item not in prefs[person] or prefs[person][item] == 0:
-                # Similarity * Score
-                totals.setdefault(item, 0)
-                totals[item] += prefs[other][item] * sim
-                # Sum of similarities
-                simSums.setdefault(item, 0)
-                simSums[item] += sim
+    def load_data(self, path='podaci/prefs.csv'):
+        movies = {}
+        for line in open('podaci/movies.csv'):
+            (movie_id, name) = line.strip().split(',', 1)
+            if ', ' in name:
+                name = name.split(', ')
+                name = name[1] + " " + name[0]
+            movies[movie_id] = name
+            # Load data
+        for line in open(path):
+            (user, movieid, rating) = line.split(',')
+            self.training_set.setdefault(user, {})
+            self.training_set[user][movies[movieid]] = float(rating)
+        self.training_set = self.transformPrefs()
 
-    # Create the normalized list
-    rankings = [(total / simSums[item], item) for item, total in totals.items()]
+    def transformPrefs(self):
+        result = {}
+        for person in self.training_set:
+            for item in self.training_set[person]:
+                result.setdefault(item, {})
 
-    # Return the sorted list
-    rankings.sort()
-    rankings.reverse()
-    return rankings
-
-
-def calculateSimilarItems(prefs, n=10):
-    # Create a dictionary of items showing which other items they
-    # are most similar to.
-    result = {}
-    # Invert the preference matrix to be item-centric
-    c = 0
-    for item in prefs:
-        # Status updates for large datasets
-        c += 1
-        if c % 100 == 0:
-            print "%d / %d" % (c, len(prefs))
-        # Find the most similar items to this one
-        scores = topMatches(prefs, item, n=n, similarity=sim_cosine)
-        result[item] = scores
-    return result
-
-
-def getRecommendedItems(itemMatch, user):
-    userRatings = user
-    scores = {}
-    totalSim = {}
-    # Loop over items rated by this user
-    for (item, rating) in userRatings.items():
-
-        # Loop over items similar to this one
-        for (similarity, item2) in itemMatch[item]:
-
-            # Ignore if this user has already rated this item
-            if item2 in userRatings:
-                continue
-            # Weighted sum of rating times similarity
-            scores.setdefault(item2, 0)
-            scores[item2] += similarity * rating
-            # Sum of all the similarities
-            totalSim.setdefault(item2, 0)
-            totalSim[item2] += similarity
-
-    # Divide each total score by total weighting to get an average
-    rankings = [(score / totalSim[item], item) for item, score in scores.items()]
-
-    # Return the rankings from highest to lowest
-    rankings.sort()
-    rankings.reverse()
-    return rankings
-
-
-def loadMovieLens(path='podaci/prefs.csv'):
-    movies = {}
-    for line in open('podaci/movies.csv'):
-        (movie_id, name) = line.strip().split(',', 1)
-        if ', ' in name:
-            name = name.split(', ')
-            name = name[1] + " " + name[0]
-        movies[movie_id] = name
-        # Load data
-    prefs = {}
-    for line in open(path):
-        (user, movieid, rating) = line.split(',')
-        prefs.setdefault(user, {})
-        prefs[user][movies[movieid]] = float(rating)
-    prefs = transformPrefs(prefs)
-    return prefs
+                # Flip item and person
+                result[item][person] = self.training_set[person][item]
+        return result
 
 
 class SlopeOne(object):
@@ -218,7 +206,7 @@ class SlopeOne(object):
             self.freqs = data
             cache.set('freqs', data, 300)
 
-    def predict(self, userprefs, num_of_movies=10):
+    def calculate_for_user(self, userprefs):
         preds, freqs = {}, {}
         for item, rating in userprefs.iteritems():
             for diffitem, diffratings in self.diffs.iteritems():
@@ -226,19 +214,19 @@ class SlopeOne(object):
                     freq = self.freqs[diffitem][item]
                 except KeyError:
                     continue
-                if freq < 10:
+                if freq < 50:
                     continue
                 preds.setdefault(diffitem, 0.0)
                 freqs.setdefault(diffitem, 0)
                 preds[diffitem] += freq * (diffratings[item] + rating)
                 freqs[diffitem] += freq
-        lista = [(value / freqs[item], item)
+        lista = {item: (value / freqs[item])
                  for item, value in preds.iteritems()
-                 if item not in userprefs and freqs[item] > 0]
-        return sorted(lista, reverse=True)[:num_of_movies]
+                 if item not in userprefs and freqs[item] > 0}
+        return lista
 
-    def compute(self, userdata):
-        #userdata = transformPrefs(userdata)
+    def load_training_data(self, userdata):
+        self.ratings = userdata.copy()
         for ratings in userdata.itervalues():
             for item1, rating1 in ratings.iteritems():
                 self.freqs.setdefault(item1, {})
@@ -251,3 +239,53 @@ class SlopeOne(object):
         for item1, ratings in self.diffs.iteritems():
             for item2 in ratings:
                 ratings[item2] /= self.freqs[item1][item2]
+
+    def predict_for_test_data(self, training_data, test_data):
+        for test_user, test_ratings in test_data.iteritems():
+            computed_ratings = self.predict(training_data[test_user])
+            for movie_name in test_ratings.keys():
+                rating = computed_ratings.get(movie_name, None)
+                if not rating:
+                    continue
+                if rating > 5.0:
+                    rating = 5.0
+                self.ratings[test_user][movie_name] = rating
+        return self.ratings
+
+    def split_dataset(self, training_set_percent=80):
+        movies = {}
+        for line in open('podaci/movies.csv'):
+            (movie_id, name) = line.strip().split(',', 1)
+            if ', ' in name:
+                name = name.split(', ')
+                name = name[1] + " " + name[0]
+            movies[movie_id] = name
+        training = {}
+        testing = {}
+        count = 0
+        training_set_items = training_set_percent * 100
+        for line in open('podaci/prefs.csv'):
+            count += 1
+            (user, movieid, rating) = line.split(',')
+            if count > training_set_items:
+                training.setdefault(user, {})
+                training[user][movies[movieid]] = float(rating)
+            else:
+                testing.setdefault(user, {})
+                testing[user][movies[movieid]] = float(rating)
+        return (training, testing)
+
+    def evaluate(self, predicted, testing):
+        real_ratings = []
+        predicted_ratings = []
+        for user, movie_ratings in testing.iteritems():
+            for movie_name, rating in movie_ratings.iteritems():
+                print movie_name
+                predicted_rating = predicted[user].get(movie_name, None)
+                if not predicted_rating:
+                    continue
+                predicted_ratings.append(predicted_rating)
+                real_ratings.append(rating)
+        print real_ratings
+        print predicted_ratings
+        return meanabs(real_ratings, predicted_ratings)
